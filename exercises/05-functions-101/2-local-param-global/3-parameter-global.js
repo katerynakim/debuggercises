ce1932399661fe82f45960223cbfc2479e4b3da5
Kateryna <katerynakim25@gmail.com>
2020-06-19 14:20:06 +0200

    completed functions 05

--- exercises/05-functions-101/2-local-param-global/3-parameter-global.js
+++ exercises/05-functions-101/2-local-param-global/3-parameter-global.js
@@ -1,24 +1,45 @@
 'use strict';
 
-let global = '<3';
+let global = 'awesome';
 
-function jazz(parameter) {
-  global = `${global} jazzy`;
-  return `${global} ${parameter}`;
+function funk() {
+  const local = 'funky'
+  global = `#${global}`;
+  return `${local} ${global}`;
 }
 
-const jazzReturn1 = jazz('hello');
-const jazz1 = jazzReturn1 === _;
-console.assert(jazz1, 'Test 1: jazz');
+const funkReturn1 = funk();
+const funkTest1 = funkReturn1 === 'funky #awesome';
+console.assert(funkTest1, 'Test 1: funk');
+
+const globalTest1 = global === '#awesome';
+console.assert(globalTest1, 'Test 2: global');
 
 global = `${global}!`;
 
-const jazzReturn2 = jazz('boat');
-const jazz2 = jazzReturn2 === _;
-console.assert(jazz2, 'Test 2: jazz');
+const globalTest2 = global === '#awesome!';
+console.assert(globalTest2, 'Test 3: global');
+
+const funkReturn2 = funk();
+const funkTest2 = funkReturn2 === 'funky ##awesome!';
+console.assert(funkTest2, 'Test 4: funk');
+
+const globalTest3 = global === '##awesome!';
+console.assert(globalTest3, 'Test 5: global');
 
 global = ':(';
 
-const jazzReturn3 = jazz('tree');
-const jazz3 = jazzReturn3 === _;
-console.assert(jazz3, 'Test 3: jazz');
+const globalTest4 = global === ':(';
+console.assert(globalTest4, 'Test 6: global');
+
+const funkReturn3 = funk();
+const funkTest3 = funkReturn3 === 'funky #:(';
+console.assert(funkTest3, 'Test 7: funk');
+
+const globalTest5 = global === '#:(';
+console.assert(globalTest5, 'Test 8: global');
+
+global = 'bye!';
+
+const globalTest6 = global === 'bye!';
+console.assert(globalTest6, 'Test 9: global');
