7d3ead8cbc8661c90200bf68b356d75b07671a12
Kateryna <katerynakim25@gmail.com>
2020-06-27 12:43:29 +0200

    completed 08-block-scope

--- exercises/08-block-scope/4-declare-and-assign/3.js
+++ exercises/08-block-scope/4-declare-and-assign/3.js
@@ -1,12 +1,12 @@
 'use strict';
 
-_;
+let m = 1;
 console.assert(m === 1, 'Test 1: m');
 {
-  _;
-  _;
+  let m = 2;
+  let l = 3;
   console.assert(m === 2, 'Test 2: m');
   console.assert(l === 3, 'Test 3: l');
 }
-_;
+m = 4;
 console.assert(m === 4, 'Test 4: m');
